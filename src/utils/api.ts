541c63478826a0717f14d4700fdce06d312183c0
Dillyn <dillynlakey@gmai.com>
2025-02-10 23:19:33 +0200

    update api root to IIS express

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5110",
+  baseURL: "https://localhost:44383",
   headers: {
     "Content-Type": "application/json",
   },
