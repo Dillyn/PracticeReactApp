bf799ba59ef4ba5f043ee9b9241ed46472901955
Dillyn <dillynlakey@gmai.com>
2025-02-12 21:23:30 +0200

    add api key

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://localhost:44383",
   headers: {
+    "X-Api-Key": "ff59f4d564f54d578ba4F133d883a776",
     "Content-Type": "application/json",
   },
 });
